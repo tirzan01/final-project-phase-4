85524816df186420993531a0dc801c9b8755ea21
Thomas Montalto <thomasmonta98@gmail.com>
2021-12-05 10:46:21 +1100

    added intial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#home'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  post '/post_like' => 'post_likes#new'
+  post '/post_like' => 'post_likes#destroy'
+
+  post '/comment_like' => 'comment_likes#new'
+  post '/comment_like' => 'comment_likes#destroy'
+
+  resources :users do
+    resources :posts
+  end
+
+  resources :posts do
+    resources :comments
+  end
+
+  resources :comments do
+    resources :comments_reply
+  end
 end
